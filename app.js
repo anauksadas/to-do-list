711a9fae2b48d2e755cc2c8570cdb762f575a376
Anauksa Das <anauksadas@yahoo.com>
2023-07-11 13:22:58 +0530

    Added .gitignore file

--- app.js
+++ app.js
@@ -158,6 +158,6 @@ async function main(){
     });
 }
 
-app.listen(3000, function(){
-    console.log("Server started on port 3000");
+app.listen(process.env.PORT || 3000 , function(){
+    console.log("Server started.");
 });
